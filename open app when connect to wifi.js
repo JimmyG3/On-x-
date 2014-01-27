fbf21c42ef443a87529ed6ceb696f37dd60e6e01
JimmyG3 <jimmykglenn@gmail.com>
2014-01-26 21:13:10 -0600

    Create open app when connect to wifi.js

---
+++ open app when connect to wifi.js
@@ -0,0 +1,22 @@
+var appName = "Google Play Music";
+var wiFi = '"belkin.900"';
+
+
+// Check for wifi connection
+device.network.on('wifiOn', function(signal) {
+    console.log('Pretest Passed');
+
+    // if wifi ssid matches your home network, launch app.
+    if(signal.ssid === wiFi) {
+        // launch app
+        var apps = device.applications;
+        apps.launch(appName);
+        console.log("Launched " + appName);
+}
+
+console.log(signal.ssid === wiFi);
+console.log('wiFi variable is ' + wiFi);
+console.log('signal ssid is' + signal.ssid);
+console.log( 'Connected to: ' + signal.ssid );
+console.log( 'Script completed successfully' );
+});
